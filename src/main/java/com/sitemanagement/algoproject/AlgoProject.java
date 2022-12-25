74e692ca31f98a820f5a0e4c6c692525d73aac25
Abdulsamet Haymana <abdulsamethaymana@gmail.com>
2022-12-25 13:30:29 +0300

    first commit

---
+++ src/main/java/com/sitemanagement/algoproject/AlgoProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sitemanagement.algoproject;
+
+/**
+ *
+ * @author tomahawk
+ */
+public class AlgoProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
